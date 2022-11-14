8178247b21c3d41617f0504809e0cc7aacebac4c
Frank Reiser <Frank@TheReisers.com>
2022-11-14 13:32:14 -0500

    Separate Serialization Routines From ByteStreambuf
    This is much cleaner as the two do not interface each other directly.

---
+++ src/Serialization.cpp
@@ -0,0 +1,8 @@
+/**
+* @file Serialization.cpp
+* @brief This file merely includes the header file which is all template code.
+* @authors Frank Reiser
+* @date Created on November 14, 2022
+*/
+
+#include "Serialization.h"
